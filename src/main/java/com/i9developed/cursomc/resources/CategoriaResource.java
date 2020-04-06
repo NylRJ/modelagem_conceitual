383ceb316fd163ae981c206efaaf5c1d3c7f4243
Moisés <loveloungeoficial@gmail.com>
2020-04-06 12:52:57 -0300

    Teste class domain category, return list category

--- src/main/java/com/i9developed/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/i9developed/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.i9developed.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.i9developed.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Esta Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 =new Categoria(1,"Informatica");
+		Categoria categoria2 =new Categoria(1,"Escritorio");
+		
+		List<Categoria> categorias =new  ArrayList<Categoria>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		return categorias;
 	}
 
 }
